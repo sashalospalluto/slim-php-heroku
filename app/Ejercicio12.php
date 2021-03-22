37a882d7f1dc12d237a509655ae496d237fc21b2
sashalospalluto <sashalospalluto@hotmail.com>
2021-03-22 20:24:03 -0300

    ejercicio12

---
+++ app/Ejercicio12.php
@@ -0,0 +1,35 @@
+<!--
+22/03/2021    
+Aplicación No 12 (Invertir palabra)
+Sasha Lospalluto
+-->
+
+<!-- Realizar el desarrollo de una función que reciba un Array de caracteres y que invierta el orden
+de las letras del Array.
+Ejemplo: Se recibe la palabra “HOLA” y luego queda “ALOH”. -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio12</title>
+</head>
+<body>
+    <?php
+
+        $palabra=array("almohadon");
+        $cant = count($palabra);
+        $palabraInvertida=array();
+
+        for ($i=$cant; $i <=0; $i++) 
+        {             
+            array_push($palabraInvertida,$palabra[$i]);
+        }
+
+        print("la palabra $palabra al reves se escribe $palabraInvertida");
+
+    ?>
+</body>
+</html>
